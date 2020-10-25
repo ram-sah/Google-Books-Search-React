6df83d975d2530726201c0f262c77d06327294c6
Ram Sah <mohan2036@gmail.com>
2020-10-24 21:56:56 -0400

    deploying page

---
+++ client/build/precache-manifest.a9502afb6000912b968bdb3359aa20aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378e3ad21abb22e9ceeedf8bc8dc3358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4e3be74bc6b17065e43",
+    "url": "/static/css/main.e6c3fc10.chunk.css"
+  },
+  {
+    "revision": "f9ac41469048978b6710",
+    "url": "/static/js/2.75b0def8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.75b0def8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e3be74bc6b17065e43",
+    "url": "/static/js/main.1ae583f0.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "1992105342c44a9429e59583cc8a4030",
+    "url": "/static/media/bookshelf.19921053.jpg"
+  }
+]);
