1fa985c57b6d4dc0f8896ab192a842daedafa415
Ram Sah <mohan2036@gmail.com>
2020-10-24 20:35:47 -0400

    Added components of pages

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+			<p>	&#169; RAM SAH</p>
+		</div>	
+	</footer>
+);
+
+export default Footer;
